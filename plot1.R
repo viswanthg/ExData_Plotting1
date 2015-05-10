d4dd8f36df9ceb065342856154923a1c70b4cdf6
Viswanth VG <viswanthg@gmail.com>
2015-05-10 11:18:25 -0500

    Assignment Code
    Assignment Code Files

---
+++ plot1.R
@@ -0,0 +1,6 @@
+ProjectData1a <- "household_power_consumption.txt"
+HPCData <- read.table(ProjectData1a, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+HPCSubSetData <- HPCData[HPCData$Date %in% c("1/2/2007","2/2/2007") ,]
+globalActivePower <- as.numeric(HPCSubSetData$Global_active_power)
+png("plot1.png", width=480, height=480)
+hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
